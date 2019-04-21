3b3db115cf6afb75ea3c5b7623dc44cfe186992c
Dmitriy Minaev <minaevdup@gmail.com>
2019-04-21 17:37:11 +0300

    refactor(components): шапка подписок в отдельный компонент

---
+++ mobile/src/components/subscribe/headScreen.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { NavigationScreenProp } from 'react-navigation';
+import {
+  View,
+  TouchableOpacity,
+  Platform,
+  StyleSheet,
+  StyleProp,
+  ViewStyle
+} from 'react-native';
+
+import Headline from '../common/Headline';
+import Ionicons from '../../navigation/TopBarIcon';
+import { authModuleName } from '../../core/constants';
+
+interface HeadScreenProps {
+  styleWrapper: StyleProp<ViewStyle>;
+  navigation: NavigationScreenProp<any, any>;
+  isAuth: boolean;
+}
+
+const HeadScreen = ({
+  styleWrapper,
+  navigation: { navigate },
+  isAuth
+}: HeadScreenProps) => {
+  return (
+    <View style={styleWrapper}>
+      <Headline text="Подписки" />
+      <TouchableOpacity
+        style={styles.icon}
+        onPress={() => authCheck({ navigate, isAuth })}
+      >
+        <Ionicons
+          name={Platform.OS === 'ios' ? 'ios-person' : 'md-person'}
+          focused={false}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const authCheck = ({
+  navigate,
+  isAuth
+}: {
+  navigate: (screen: string) => any;
+  isAuth: boolean;
+}) => {
+  if (isAuth) {
+    navigate('Profile');
+  } else {
+    navigate('SignIn');
+  }
+};
+
+const styles = StyleSheet.create({
+  icon: {
+    paddingLeft: 40
+  }
+});
+
+export default connect((state: any) => ({
+  isAuth: !!state[authModuleName].token
+}))(HeadScreen);
--- mobile/src/screens/SubscribeScreen.tsx
+++ mobile/src/screens/SubscribeScreen.tsx
@@ -1,56 +1,23 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { View, StyleSheet, Platform, TouchableOpacity } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { NavigationScreenProp } from 'react-navigation';
 
 import { wrapperBottomTabNavigator as wrapper } from '../constants/Style';
-import Headline from '../components/common/Headline';
-import Ionicons from '../navigation/TopBarIcon';
 import { headSubscribeHomeScreen } from '../constants/Style';
-import { authModuleName } from '../core/constants';
+import HeadScreen from '../components/subscribe/headScreen';
 
 interface HomeScreenProps {
   navigation: NavigationScreenProp<any, any>;
   userToken: string;
 }
 
-const HomeScreen = ({
-  navigation: { navigate },
-  userToken
-}: HomeScreenProps) => {
+const HomeScreen = ({ navigation }: HomeScreenProps) => {
   return (
     <View style={wrapper}>
-      <View style={styles.head}>
-        <Headline text="Подписки" />
-        <TouchableOpacity
-          style={styles.icon}
-          onPress={() => authCheck({ navigate, userToken })}
-        >
-          <Ionicons
-            name={Platform.OS === 'ios' ? 'ios-person' : 'md-person'}
-            focused={false}
-          />
-        </TouchableOpacity>
-      </View>
-
+      <HeadScreen styleWrapper={styles.head} navigation={navigation} />
       <View style={styles.body} />
     </View>
   );
-};
-
-const authCheck = ({
-  navigate,
-  userToken
-}: {
-  navigate: (screen: string) => any;
-  userToken: string;
-}) => {
-  console.log(123123131312, userToken);
-  if (userToken) {
-    navigate('Profile');
-  } else {
-    navigate('SignIn');
-  }
 };
 
 const styles = StyleSheet.create({
@@ -65,14 +32,7 @@ const styles = StyleSheet.create({
   },
   body: {
     height: '92%'
-  },
-  icon: {
-    paddingLeft: 40
   }
 });
 
-const mapStateToProps = (state: any) => ({
-  userToken: state[authModuleName]
-});
-
-export default connect(mapStateToProps)(HomeScreen);
+export default HomeScreen;
